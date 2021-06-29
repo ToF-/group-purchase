c276e0f6b6af6b92254519323dcbecef023fd560
Christophe Thibaut <cthibaut@octo.com>
2021-06-29 09:24:20 +0200

    init carpaccio list

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,5 @@
 module Main where
 
-import GroupPurchase
-import System.Environment
-
 main :: IO ()
 main = do
-    args <- getArgs
-    groupPurchase (args!!0) (args!!1)
+    undefined
---
+++ carpaccio.md
@@ -0,0 +1,5 @@
+# Carpaccio
+## Reading some text content, printing it back
+## Reading some csv content (orders), printing the values
+## Printing csv content (amounts)
+
