05b0f62ccb97267c34f7fa56631c97ad00bdc102
Christophe Thibaut <cthibaut@octo.com>
2021-06-30 21:29:47 +0200

    added controls over csv records for Orders

--- src/Order.hs
+++ src/Order.hs
@@ -3,16 +3,26 @@ module Order
     where
 
 import Data.Text
+import Item
 import Data.Csv
+import Data.Vector
+import Data.ByteString.Lazy.Char8 as BS
 
-data Order = Order { item :: Text
+
+data Order = Order { item :: Item
                    , unitPrice :: Double
                    , quantity :: Integer
                    , buyer :: Text }
     deriving (Eq, Show)
 
 instance FromNamedRecord Order
-    where parseNamedRecord r = Order <$> (strip <$> r .: "item")
+    where parseNamedRecord r = Order <$> (r .: "item")
                                      <*> r .: "unitp"
                                      <*> r .: "qty"
                                      <*> (strip <$> r.: "buyer")
+
+decodeOrders :: ByteString -> Either String (Header, Vector Order)
+decodeOrders = decodeByName 
+    . BS.unlines
+    . Prelude.filter (\l -> not (BS.null l) && BS.head l /= '~')
+    . BS.lines
--- test/OrderSpec.hs
+++ test/OrderSpec.hs
@@ -3,6 +3,7 @@ module OrderSpec
     where
 
 import Test.Hspec
+import Item
 import Order
 import Data.ByteString.Lazy.Char8 as BS
 import Data.Vector
@@ -11,13 +12,37 @@ import Data.Csv
 spec :: SpecWith ()
 spec = do
     describe "Order" $ do
+        let expectedHeader = fromList ["item","unitp","qty","amount","buyer"]
+
         it "can be read from a csv ByteString" $ do
             let csv = BS.unlines ["item,unitp,qty,amount,buyer"
                                  ,"pencils           ,  0.50 , 20  ,  10.00 , Bertrand"]
-                result = decodeByName csv
-            result `shouldBe`
-                Right (fromList ["item","unitp","qty","amount","buyer"]
-                      ,fromList [Order { item = "pencils"
+            decodeOrders csv `shouldBe`
+                Right (expectedHeader,
+                       fromList [Order { item = Item "pencils"
+                                       , unitPrice = 0.50
+                                       , quantity = 20
+                                       , buyer = "Bertrand" }])
+
+        it "can't be created from a csv record with an item starting with ~" $ do
+            let csv = BS.unlines ["item,unitp,qty,amount,buyer"
+                                 ,"~ total, , , 10.00, "]
+            decodeOrders csv `shouldBe` Right (expectedHeader, fromList[])
+
+        it "can't be created from a csv record with an empty item" $ do
+            let csv = BS.unlines ["item,unitp,qty,amount,buyer"
+                                 ,""
+                                 ,",1 ,1 , 10.00, "]
+            decodeOrders csv `shouldBe`
+                Left "parse error (Failed reading: conversion error: empty Item field) at \"\\n\""
+
+        it "can be created from a csv file where ~ records are filtered out" $ do
+            let csv = BS.unlines ["item,unitp,qty,amount,buyer"
+                                 ,"pencils           ,  0.50 , 20  ,  10.00 , Bertrand"
+                                 ,"~ shipping        , 30.00 , 1   ,        , "]
+            decodeOrders csv `shouldBe`
+                Right (expectedHeader,
+                       fromList [Order { item = Item "pencils"
                                        , unitPrice = 0.50
                                        , quantity = 20
                                        , buyer = "Bertrand" }])
