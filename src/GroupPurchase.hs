cb9332471ab93347a35aa037a60ecbcc739bd291
Christophe Thibaut <cthibaut@octo.com>
2021-07-02 22:31:55 +0200

    groupPurchase adjusts the amounts so that the sum of the rounded parts is equal to the total

--- src/GroupPurchase.hs
+++ src/GroupPurchase.hs
@@ -2,14 +2,16 @@ module GroupPurchase
     where
 
 import Bill
+import CascadeRound
 import Money
 import Data.List as L
 
 applyShipping :: Money -> [Bill] -> [Bill]
-applyShipping shipping bills = L.zipWith setAmount bills withShipping
+applyShipping shipping bills = L.zipWith setAmount bills withShippingRounded
     where
     setAmount :: Bill -> Double -> Bill
     setAmount b d = b { amount = money d }
+    withShippingRounded = L.map ((/100).fromIntegral) $ cascadeRound $ L.map (*100) withShipping
     withoutShipping = L.map (asDouble . amount) bills
     total = L.sum withoutShipping
     fee = asDouble shipping
--- test/GroupPurchaseSpec.hs
+++ test/GroupPurchaseSpec.hs
@@ -19,5 +19,12 @@ spec = do
                 ,bill "Clara" 77.0
                 ,bill "Desmond" 22.0]
 
-        -- it "adjusts amounts so that the sum of amounts equals the total" $ do
+        it "adjusts amounts so that the sum of amounts equals the total" $ do
+            let bills = [bill "Bertrand" 33.0
+                        ,bill "Clara" 33.0
+                        ,bill "Desmond" 33.0]
+            applyShipping (money 1.0) bills `shouldBe`
+                [bill "Bertrand" 33.33
+                ,bill "Clara" 33.34
+                ,bill "Desmond" 33.33]
 
