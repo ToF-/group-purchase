12ec07428560bc6010198d09169e760333706329
Christophe Thibaut <cthibaut@octo.com>
2021-07-02 21:15:26 +0200

    Money can be multiplied n times, n being an integer

--- src/Money.hs
+++ src/Money.hs
@@ -4,6 +4,12 @@ module Money
 data Money = Money { value :: Integer }
     deriving (Eq, Show)
 
+instance Num Money where
+    m * n = Money ((value m) * (value n) `div` 100)
+
 money :: Double -> Money
-money = Money . round
+money = Money . round . (* 100)
+
+times :: Integer -> Money -> Money
+times n m = m * (money (fromIntegral n))
 
